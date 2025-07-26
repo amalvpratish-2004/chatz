7ee1e5fabfdfe9db36d930e80bc89a0c276ac234
Amal VP <145320734+amalvpratish-2004@users.noreply.github.com>
2025-07-26 11:34:55 +0530

    login functionalities implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRouter from './routes/auth.route.js';
+import messageRouter from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use('/api/auth', authRouter);
+app.use('/api/message', messageRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
