237967dd0e1c3a0d7d214e007f7120a8774d15c6
Amal VP <145320734+amalvpratish-2004@users.noreply.github.com>
2025-07-27 12:28:29 +0530

    deployment version

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 import authRouter from './routes/auth.route.js';
 import messageRouter from './routes/message.route.js';
@@ -9,6 +10,8 @@ import { connectDB } from './lib/db.js';
 import { app, server } from './lib/socket.js';
 
 dotenv.config();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,7 +23,14 @@ app.use(cors({
 app.use('/api/auth', authRouter);
 app.use('/api/messages', messageRouter);
 
-const PORT = process.env.PORT;
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
+
 server.listen(PORT, () => {
     console.log('Server listening at port ', + PORT);
     connectDB();
